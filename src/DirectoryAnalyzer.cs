377fee4cbfb2e2e1a05205af45fde6168036a8fc
Arturo Torres <15116884+ArtTorres@users.noreply.github.com>
2023-11-17 22:44:04 -0600

    Adding support to drive analysis

--- src/DirectoryAnalyzer.cs
+++ src/DirectoryAnalyzer.cs
@@ -1,10 +1,45 @@
-﻿namespace DirectorySize
+﻿using System.IO;
+
+namespace DirectorySize
 {
     internal class DirectoryAnalyzer
     {
         public static IEnumerable<SizeInfo> Analyze(string directoryPath)
         {
-            return Analyze(new DirectoryInfo(directoryPath));
+            var directory = new DirectoryInfo(directoryPath);
+
+            if (directory.Exists)
+            {
+                return Analyze(directory);
+            }
+            else
+            {
+                var drive = new DriveInfo(directoryPath);
+                return Analyze(drive);
+            }
+        }
+
+        public static IEnumerable<SizeInfo> Analyze(DriveInfo drive)
+        {
+            foreach (var dir in drive.RootDirectory.GetDirectories())
+            {
+                yield return new SizeInfo()
+                {
+                    Id = dir.Name,
+                    Size = Evaluate(dir),
+                    Type = FileType.Directory
+                };
+            }
+
+            foreach (var file in drive.RootDirectory.GetFiles())
+            {
+                yield return new SizeInfo()
+                {
+                    Id = file.Name,
+                    Size = file.Length,
+                    Type = FileType.File
+                };
+            }
         }
 
         public static IEnumerable<SizeInfo> Analyze(DirectoryInfo directory)
